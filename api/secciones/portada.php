a152721723de2b2ccfe62fc6b9788b6dca8849f9
jesuscazorla <72396901+jesuscazorla@users.noreply.github.com>
2024-10-22 19:18:23 +0200

    Cambio del titulo de la portada

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "La portada CC2022",
-"autor" => "mtorres",
-"resumen" => "El resumen maravilloso",
+"titulo" => "Portada CC 2025",
+"autor" => "jcg449",
+"resumen" => "Resumen de la portada jcg449 ",
 ];
 ?>
